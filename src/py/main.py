5bcac85f377a2f3ba366f52c34f9788d8b3b2fe8
Adijaa <khadija.adijadija@gmail.com>
2026-01-28 20:04:17 +0100

    Implement FastAPI endpoints for mathematical functions
    domain , intersections , symmetires , derivates 
    no sign

--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,111 @@
 # Prototipo
 # https://gist.github.com/maziogra/fb4f5f1807a2e6dd6a9a72c858b81580
+
+from fastapi import FastAPI
+import sympy as sp
+from py.Domain.domain import domain # verificato
+from py.Intercepts.intersections import intersections #verificato 
+import py.Utils.findPeriod as findPeriod
+from py.Symmetries.Symmetries import symmetries # veirificato
+from py.Derivatives.explainDerivatives import explainDerivatives #verificato
+app = FastAPI()
+
+@app.get("/domain")
+async def get_domain(f: str | None = None):
+    if f == None:
+        return {"msg": "No function was provided"}
+    
+    else:
+        x = sp.symbols("x")
+        expr = sp.parse_expr(f, evaluate=True)
+
+        if str(expr) == "zoo":
+            return {"msg": "Division by zero"}
+        
+        for i in expr.free_symbols:
+            if i != x:
+                return {"msg": "Function is not correctly formatted"}
+        dominio = domain(expr)
+        
+        return {
+            "msg": "OK",
+            "domain": str(dominio),
+        }
+    
+@app.get("/intersections")
+async def get_intersections(f: str | None = None):
+    if f == None:
+        return {"msg": "No function was provided"}
+    
+    else:
+        x = sp.symbols("x")
+        expr = sp.parse_expr(f, evaluate=True)
+
+        if str(expr) == "zoo":
+            return {"msg": "Division by zero"}
+        
+        for i in expr.free_symbols:
+            if i != x:
+                return {"msg": "Function is not correctly formatted"}
+        
+        result = intersections(expr)
+        
+        punti = []
+        for item in result:
+            if not isinstance(item, str):
+                punti.append(str(item))
+            else:
+                punti.append(item)
+        
+        return {
+            "msg": "OK",
+            "punti": punti,
+        }
+        
+@app.get("/symmetries")
+async def get_symmetries(f: str | None = None):
+    if f == None:
+        return {"msg": "No function was provided"}
+    
+    else:
+        x = sp.symbols("x")
+        expr = sp.parse_expr(f, evaluate=True)
+
+        if str(expr) == "zoo":
+            return {"msg": "Division by zero"}
+        
+        for i in expr.free_symbols:
+            if i != x:
+                return {"msg": "Function is not correctly formatted"}
+        
+        simmetria = symmetries(expr)
+        
+        return {
+            "msg": "OK",
+            "symmetry": simmetria,
+        }
+        
+
+
+@app.get("/derivatives")
+async def get_derivatives(f: str | None = None):
+    if f == None:
+        return {"msg": "No function was provided"}
+    
+    else:
+        x = sp.symbols("x")
+        expr = sp.parse_expr(f, evaluate=True)
+
+        if str(expr) == "zoo":
+            return {"msg": "Division by zero"}
+        
+        for i in expr.free_symbols:
+            if i != x:
+                return {"msg": "Function is not correctly formatted"}
+        
+        derivative = explainDerivatives(expr, x)
+        
+        return {
+            "msg": "OK",
+            "derivative": str(derivative),
+        }
